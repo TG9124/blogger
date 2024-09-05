0c13676fa847bbea93f4644e650c5d0f5100acbc
TG9124 <tarung9124@gmail.com>
2024-09-05 23:08:45 +0530

    first commit

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="F:\HCL\blogger\client\ML logo.png" alt="Logo"></img>
+        </div>
+        <div className="texts">
+          <h2>My happy place</h2>
+          <p className="info">
+            <a className="author">Tarun G</a>
+            <time>2024-09-05</time>
+          </p>
+          <p>Today at its special launch event</p>
+        </div>
+      </div>
+    )
+}
